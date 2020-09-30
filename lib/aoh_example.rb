ca9369779f9d2ad18aa9c1c1528abe5827769ed6
Luis Torres <luistjr@gmail.com>
2020-09-30 20:06:11 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -32,7 +32,7 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
     {:name => "Joelle van Dyne", :occupation => "Radio Personality" },
     {:name => "Pat Monteseian", :occupation => "Staff" },
     {:name => "Kate Gompert", :occupation => "None" },
-    {:name => "Bruce Green", :occupation => "Fan of Mildred" },
+    {:name => "Bruce Green", :occupation => "Fan of Mildred" }
     ]
 end
 
